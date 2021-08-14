08b217b7a3343971122294febdbd9402bb86d6e7
Dulitha <47850462+dulithaks@users.noreply.github.com>
2021-08-14 19:40:16 +0530

    Remove sanctum and keep basic token auth

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [\App\Http\Controllers\LoginController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
