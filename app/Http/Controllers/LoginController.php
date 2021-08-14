8ed3978c3c52d87c619a335454d94732bdd21127
Dulitha <47850462+dulithaks@users.noreply.github.com>
2021-08-15 01:10:01 +0530

    Remove Sanctum configuration

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
